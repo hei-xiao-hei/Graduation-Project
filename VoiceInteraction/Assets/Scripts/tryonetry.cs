74289b932ea6ee457627ad311d73a6fe2e710db8
hei-xiao-hei <2668841126@qq.com>
2023-04-05 11:30:57 +0800

    4.5
    new project

---
+++ VoiceInteraction/Assets/Scripts/tryonetry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tryonetry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("成功的第一步，新建项目");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
